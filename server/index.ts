302626de23a71a741ad027b19016a11e79ac532e
ashish <ashiiiyadav46@gmail.com>
2025-05-09 11:51:52 +0530

    FIRST COMMIT

--- server/index.ts
+++ server/index.ts
@@ -62,9 +62,9 @@ app.use((req, res, next) => {
   const port = 5000;
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1", // or just remove 'host' to default to localhost
   }, () => {
-    log(`serving on port ${port}`);
+    log(`🚀 Server is running at: http://localhost:${port}`);
+
   });
 })();
